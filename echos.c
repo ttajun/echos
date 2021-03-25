32291988f874b8fac5835409c4fdc85bead28665
Jung Tae Jun <ttajun@gmail.com>
2021-03-25 11:10:53 +0900

    change: echos.c

--- echos.c
+++ echos.c
@@ -1,16 +1,15 @@
 #include <signal.h>
+#include <syslog.h>
 #include <sys/wait.h>
-#include "inet_sockets.h"       /* Declarations of inet*() socket functions */
+#include "inet_sockets.h"
 #include "common.h"
 #include "become_daemon.h"
 
-#define SERVICE "echo"          /* Name of TCP service */
 #define BUF_SIZE 4096
 
-static void             /* SIGCHLD handler to reap dead child processes */
-grimReaper(int sig)
+static void removeZombie(int sig)
 {
-    int savedErrno;             /* Save 'errno' in case changed here */
+    int savedErrno;             /* waitpid 때문에 errno 변경 방지 */
 
     savedErrno = errno;
     while (waitpid(-1, NULL, WNOHANG) > 0)
@@ -18,23 +17,21 @@ grimReaper(int sig)
     errno = savedErrno;
 }
 
-/* Handle a client request: copy socket input back to socket */
-
-static void
-handleRequest(int cfd)
+static void handleRequest(int cfd)
 {
     char buf[BUF_SIZE];
     ssize_t numRead;
 
     while ((numRead = read(cfd, buf, BUF_SIZE)) > 0) {
         if (write(cfd, buf, numRead) != numRead) {
-            fprintf(stderr, "write() failed: %s", strerror(errno));
+            /* log 는 /var/log/messages */
+            syslog(LOG_ERR, "write() failed: %s", strerror(errno));
             exit(EXIT_FAILURE);
         }
     }
 
     if (numRead == -1) {
-        fprintf(stderr, "Error from read(): %s", strerror(errno));
+        syslog(LOG_ERR, "Error from read(): %s", strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
@@ -47,12 +44,14 @@ int main(int argc, char *argv[])
     if (argc != 2 || strcmp(argv[1], "--help") == 0)
         usageErr("%s port\n", argv[0]);
 
+    /* deamonize */
     if (becomeDaemon(0) == -1)
         errExit("becomeDaemon");
 
+    /* signal handler for remove zombie */
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
-    sa.sa_handler = grimReaper;
+    sa.sa_handler = removeZombie;
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
         fprintf(stderr, "Error from sigaction(): %s", strerror(errno));
         exit(EXIT_FAILURE);
@@ -71,22 +70,21 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
 
-        /* Handle each client request in a new child process */
-
+        /* 요청이 올때마다 새로운 child 생성*/
         switch (fork()) {
         case -1:
             fprintf(stderr, "Can't create child (%s)", strerror(errno));
-            close(cfd);                 /* Give up on this client */
-            break;                      /* May be temporary; try next client */
+            close(cfd);
+            break;
 
         case 0:                         /* Child */
-            close(lfd);                 /* Unneeded copy of listening socket */
+            close(lfd);                 /* Child 는 listening socket 필요없음 */
             handleRequest(cfd);
             _exit(EXIT_SUCCESS);
 
         default:                        /* Parent */
-            close(cfd);                 /* Unneeded copy of connected socket */
-            break;                      /* Loop to accept next connection */
+            close(cfd);                 /* Parent 는 connected socket 필요없음 */
+            break;
         }
     }
 }
